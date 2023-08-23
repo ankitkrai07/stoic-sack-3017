57c96ff1c6c3c21c4a7119f080e235b82930e760
unknown <sachinpatel.k.786@gami.com>
2023-08-23 23:21:22 +0530

    GGYHG

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { LandingPage } from "./LandingPage";
+import Calculators from "./Calculators";
 
 export const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/calculators" element={<Calculators />} />
       <Route path="*" element={<h2>404 Page not found!</h2>} />
     </Routes>
   );
