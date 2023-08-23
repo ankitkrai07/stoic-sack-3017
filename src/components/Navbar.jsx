42447ccdcbfc01ba5859a3cf065f46b0d09dd7a8
Ankit Kumar <ankitkrai4n0522@gmail.com>
2023-08-23 23:17:54 +0530

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,74 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <div className="top-nav">
+        <div className="reg-nav">
+          <div>
+            <p>Registered for SARS eFiling? Click here to get started.</p>
+          </div>
+        </div>
+        <div className="link-div">
+          <div>
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </div>
+          <div>
+            <Link className="link" to="/contact">
+              Contact
+            </Link>
+          </div>
+          <div>
+            <Link className="link" to="/faq">
+              FAQ
+            </Link>
+          </div>
+          <div>
+            <Link className="link" to="/calculators">
+              Calculators
+            </Link>
+          </div>
+          <div>
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="top-nav-II">
+        <div className="logo-img">
+          <Link to="/">
+            <img
+              src="https://www.taxtim.com/za/images/taxtim-logo.svg"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="top-nav-III">
+          <div>
+            <a href="/howitworks" className="anch">
+              How it works
+            </a>
+          </div>
+          <div>
+            <a href="/" className="anch">
+              Pricing
+            </a>
+          </div>
+          <div>
+            <a href="/" className="anch">
+              Help
+            </a>
+          </div>
+          <div>
+            <button className="getstart">GET STARTED</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
