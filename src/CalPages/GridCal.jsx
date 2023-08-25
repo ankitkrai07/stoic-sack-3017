db6914bdf0cce10ad88e0cea9ec2fdbf6e431810
unknown <sachinpatel.k.786@gami.com>
2023-08-25 10:24:19 +0530

    add calculators

---
+++ src/CalPages/GridCal.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {styled} from "styled-components"
+export default function GridCal() {
+  return (
+    <DIV>GridCal</DIV>
+  )
+}
+const DIV=styled.div`
+width: 70%;
+`
--- src/CalPages/Topsection.jsx
+++ src/CalPages/Topsection.jsx
@@ -1,21 +1,38 @@
 import React from "react";
+import { styled } from "styled-components";
 
 export default function () {
-  return (<div style={{height:"250px",backgroundColor:"#eefafc"}}>
+  return (<DIV >
     <div style={{display:"flex",justifyContent:"flex-end",margin:"auto",width:"80%"}}>
     <div style={{width:"80%"}}>
-        <h1>Do your tax with TaxTim and WIN R10,000</h1>
-        <span> T&C's apply</span>
+        <h2>Do your tax with EzTax and WIN R10,000  <span style={{fontSize:"12px"}}> T&C's apply</span></h2>
+       
         <h4 >How do you currently do your tax?</h4>
-        <button style={{margin:"0px 0px 10px"}}>SARS EFILING</button>
-        <button style={{margin:"0px 10px"}}>TAX PRACTITIONER</button>
-        <button style={{margin:"0px 10px"}}>AUTO-ASSESSMENT</button>
-        <button style={{margin:"0px 10px"}}>NEW TO TAX</button>
+        <div >
+        <button >SARS EFILING</button>
+        <button>TAX PRACTITIONER</button>
+        <button >AUTO-ASSESSMENT</button>
+        <button >NEW TO TAX</button>
+        </div>
       </div>
       <div style={{margin:"auto"}}>
         <img src="https://media.taxtim.com/images/media-za/reclining-man.png" alt="" style={{width:"50%",alignItems:"center",marginLeft:"30%"}}/>
       </div>
     </div>
-    </div>
+    </DIV>
   );
 }
+const DIV=styled.div`
+/* style={{height:"250px",backgroundColor:"#eefafc"}} */
+height: 250px;
+background-color: #eefafc;
+button{
+    margin: 5px 0px 0px 10px;
+    height: 30px;
+    background-color: #4b4848;
+    color: white;
+    border-radius: 10px;
+    border: none;
+
+}
+`
