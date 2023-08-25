9634b4ebc3d1088b23f93c6cdec7e10e5a30373e
Ankit Kumar <ankitkrai4n0522@gmail.com>
2023-08-25 23:38:59 +0530

    Avtar

---
+++ src/chatboat/Avatar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import { styles } from "./styles";
+
+const Avatar = (props) => {
+  const [hover, setHover] = useState(false);
+
+  console.log(props.visible);
+  return (
+    <div style={props.style}>
+      <div
+        className="transition-3"
+        style={{
+          ...styles.avatarHello,
+          ...{ opacity: hover ? "1" : "0" },
+        }}
+      >
+        Hey its Tim!
+      </div>
+
+      <div
+        className="transition-3"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        onClick={() => props.onClick && props.onClick()}
+        style={{
+          ...styles.chatWithMeButton,
+          ...{ border: hover ? " 2px solid green" : "2px solid red" },
+        }}
+      />
+    </div>
+  );
+};
+export default Avatar;
