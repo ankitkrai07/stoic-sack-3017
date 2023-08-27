27add46d438b41bac8db7df3f07108769e863eed
Ankit Kumar <ankitkrai4n0522@gmail.com>
2023-08-27 20:04:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  
     <Provider store={store}>
       <App />
     </Provider>
